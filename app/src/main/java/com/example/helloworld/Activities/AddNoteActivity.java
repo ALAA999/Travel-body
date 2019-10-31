0fe5800815dea4bde6ebed974045492b66d195e0
yahia1997ezat <yahia1997ezat@gmail.com>
2019-10-31 13:37:12 +0200

    edit comment

--- app/src/main/java/com/example/helloworld/Activities/AddNoteActivity.java
+++ app/src/main/java/com/example/helloworld/Activities/AddNoteActivity.java
@@ -30,7 +30,7 @@ public class AddNoteActivity extends AppCompatActivity {
     private Button btLocation, btSave;
     private EditText etTitle, etTime, etDescription;
     private AppDatabase mDb;
-    //hhhhhhhh
+    //hhhhhhhh2222
 
     @Override
     protected void onResume() {
